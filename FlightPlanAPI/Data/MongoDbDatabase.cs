159243d733037a29bf12800826c6adc08ff7ab03
husseab <abdikarim.hussein557@gmail.com>
2023-07-02 21:27:36 +0100

    Add project files.

---
+++ FlightPlanAPI/Data/MongoDbDatabase.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using FlightPlanAPI.Models;
+
+namespace FlightPlanAPI.Data
+{
+    public class MongoDbDatabase
+    {
+       private IMongoCollection<BsonDocument> GetCollection(
+            string databaseName, string collectionName)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase(databaseName);
+
+        }
+    }
+}
